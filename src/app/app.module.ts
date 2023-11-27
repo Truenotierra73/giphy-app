5e87f10995b9ce9220747eb45d0be0197779124f
Agustin Bollati <bollatiagustin@gmail.com>
2023-11-27 17:44:00 -0300

    creacion de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment.development';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  firebase: {
+    projectId: 'mobydigitaltest-75019',
+    appId: '1:316937901291:web:22fbe48ab331aeabf45aae',
+    storageBucket: 'mobydigitaltest-75019.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAt4Kz1yCaGVcxG-F-2sRpY4N6r55lRJpA',
+    authDomain: 'mobydigitaltest-75019.firebaseapp.com',
+    messagingSenderId: '316937901291'
+  },
+  giphy: {
+    apiKey: '54ktqLmonn7ejAUACxhz3AGOSGL5Ss4D',
+    gifURL: 'api.giphy.com/v1/gifs',
+    stickerURL: 'api.giphy.com/v1/stickers'
+  }
+};
